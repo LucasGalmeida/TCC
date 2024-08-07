09128b943353d2c0d8915e23aac17519b47934be
LucasGalmeida <lucas_candeias@hotmail.com>
2024-08-07 07:25:12 -0300

    feature/inicio antd e roteamento

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,36 +1,103 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
 import Login from './view/Login';
 import Register from './view/Register';
-import { Layout } from 'antd';
+import { Button, Layout, Menu, theme } from 'antd';
 import { Content, Footer, Header } from 'antd/es/layout/layout';
 import Chat from './view/Chat';
+import ErrorPage from './view/NotFound';
+import { useState } from 'react';
+import Sider from 'antd/es/layout/Sider';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+
 
 function App() {
+
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/chat",
+      element: <Chat />,
+      errorElement: <ErrorPage />,
+    }
+  ])
 
   return (
     <>
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-        </Header>
-        <Content>
-          <div className="site-layout-content">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/chat" element={<Chat />} />
-            </Routes>
-          </BrowserRouter>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="demo-logo-vertical" >
+            <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              items={[
+                {
+                  key: '1',
+                  icon: <UserOutlined />,
+                  label: 'nav 1',
+                },
+                {
+                  key: '2',
+                  icon: <VideoCameraOutlined />,
+                  label: 'nav 2',
+                },
+                {
+                  key: '3',
+                  icon: <UploadOutlined />,
+                  label: 'nav 3',
+                },
+              ]}
+            />
           </div>
+        </Sider>
+        <Layout>
+        <Header style={{ padding: 0 }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+              color: "white",
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <RouterProvider router={router} />
         </Content>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2024 Created by Ant UED</Footer>
       </Layout>
-
-
-      
-       
+    </Layout>
     </>
   )
 }
